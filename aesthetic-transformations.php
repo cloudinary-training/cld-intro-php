21bf91f93ca1c9aba0be3202e9002a9a87e2bc50
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-03-29 10:11:35 -0700

    updating aestheic and version of sdk

--- aesthetic-transformations.php
+++ aesthetic-transformations.php
@@ -22,6 +22,7 @@ use Cloudinary\Transformation\Source;
 use Cloudinary\Transformation\Position;
 use Cloudinary\Transformation\RoundCorners;
 use Cloudinary\Transformation\Compass;
+use Cloudinary\Transformation\VideoSource;
 
 
 # Config
@@ -30,53 +31,53 @@ use Cloudinary\Transformation\Compass;
 # Constructor
 // # $cloudinary = new Cloudinary('cloudinary://API_KEY:API_SECRET@CLOUD_NAME');
 $cloudinary = new Cloudinary();
-echo $cloudinary->configuration -> cloud ->  . "\n";
-// $cloudinary->configuration->url->analytics(false);
+$cloudinary->configuration->url->analytics(false);
+echo $cloudinary->configuration -> cloud -> cloudName . "\n";
+$cloudinary->configuration->url->analytics(false);
 
 
-# Radius - generate a circle for 1:1 aspect ratio and png format
+
+# Round Corners (think CSS radius) - generate a circle for 1:1 aspect ratio and png format
 
 // echo $cloudinary->image('dog.png')
 //   ->resize(Resize::thumbnail(300,300,Gravity::auto()))
 //   ->roundCorners(CornerRadius::max())
 //   ->quality(Quality::auto()) . "\n";
 
-# Borders - wait for release
-// with addParams
-echo $cloudinary->image('blackberry')
-    ->resize(Resize::thumbnail(300, 300, Gravity::auto()))
-    ->roundCorners(RoundCorners::max()
-        ->addQualifier(Border::solid()
-            ->width(10)
-            ->color(Color::rgb('#bde4fb'))))
-    ->format(Format::png())
-    ->quality(Quality::auto())  . "\n";
+# Next 2 sample yield the same transformation
+# with addQualifier - if there was a new qualifier this could give you early access ???? correct ???
+// echo $cloudinary->image('blackberry')
+//     ->resize(Resize::thumbnail(300, 300, Gravity::auto()))
+//     ->roundCorners(RoundCorners::max()
+//         ->addQualifier(Border::solid()
+//             ->width(10)
+//             ->color(Color::rgb('#bde4fb'))))
+//     ->format(Format::png())
+//     ->quality(Quality::auto())  . "\n";
 
-// with border action fn to be implemented
-echo $cloudinary->image('blackberry')
-    // ->resize(Resize::thumbnail(300, 300, Gravity::auto()))
-    ->resize(Resize::thumbnail()
-        ->height(300)
-        ->width(300)
-        ->gravity(Gravity::auto()))
-    ->border(Border::solid()->width(10)
-        ->color(Color::rgb('#bde4fb'))
-        ->roundCorners(RoundCorners::max()))
-    ->format(Format::png())
-    ->version(123)
-    ->quality(Quality::auto())  . "\n";
+# with border action 
+// echo 
+//     $cloudinary->image('blackberry')
+//     ->resize(Resize::thumbnail()
+//         ->height(300)
+//         ->width(300)
+//         ->gravity(Gravity::auto()))
+//     ->border(Border::solid()->width(10)
+//         ->color(Color::rgb('#bde4fb'))
+//         ->roundCorners(RoundCorners::max()))
+//     ->format(Format::png())
+//     ->quality(Quality::auto())
+//     . "\n";
 
-// add generic using raw transformation
-echo $cloudinary->image('blackberry')
-    // ->addTransformation("some new transformation")
-    ->addTransformation("c_thumb,g_auto,h_300,w_300/bo_10px_solid_rgb:bde4fb,r_max/f_png/q_auto")
-    ->version(123) . "\n";
+# add generic using raw transformation
+// echo $cloudinary->image('blackberry')
+//     // ->addTransformation("some new transformation")
+//     ->addTransformation("c_thumb,g_auto,h_300,w_300/bo_10px_solid_rgb:bde4fb,r_max/f_png/q_auto")
+//     ->version(123) . "\n";
 
 
 # Background for padding
-# can't use gravity auto with pad but you can direct the location of the padding
-
-// waiting for fix
+# you don't need gravity with padding because you'll get the whole image
 // echo ($cloudinary->image('face')
 //   ->resize(Resize::pad(300,200,Background::auto()))
 //   ->border(Border::solid()->width(10)->color(Color::WHITE))
@@ -92,29 +93,29 @@ echo $cloudinary->image('blackberry')
 //   ->quality(Quality::auto())
 //   ->format(Format::png()) . "\n");
 
-
+# in the next 3 we see that you can't add 2 effect in same action
 # tint
-echo ($cloudinary->image('face')
-    ->resize(Resize::thumbnail(300, 300, Gravity::auto()))
-    ->adjust(Adjust::tint(40, Color::MAGENTA))
-    ->quality(Quality::auto())
-    ->format(Format::auto()) . "\n");
+// echo ($cloudinary->image('face')
+//     ->resize(Resize::thumbnail(300, 300, Gravity::auto()))
+//     ->adjust(Adjust::tint(40, Color::MAGENTA))
+//     ->quality(Quality::auto())
+//     ->format(Format::auto()) . "\n");
 
 # grayscale
-echo ($cloudinary->image('face')
-    ->resize(Resize::thumbnail(300, 300, Gravity::auto()))
-    ->effect(Effect::grayscale())
-    ->quality(Quality::auto())
-    ->format(Format::auto()) . "\n");
+// echo ($cloudinary->image('face')
+//     ->resize(Resize::thumbnail(300, 300, Gravity::auto()))
+//     ->effect(Effect::grayscale())
+//     ->quality(Quality::auto())
+//     ->format(Format::auto()) . "\n");
 
 # duotone = grayscale + tint in chained transformation
 # experiment with color and amount
-echo ($cloudinary->image('face')
-    ->resize(Resize::thumbnail(300, 300, Gravity::auto()))
-    ->effect(Effect::grayscale())
-    ->adjust(Adjust::tint(20, Color::MAGENTA))
-    ->quality(Quality::auto())
-    ->format(Format::auto()) . "\n");
+// echo ($cloudinary->image('face')
+//     ->resize(Resize::thumbnail(300, 300, Gravity::auto()))
+//     ->effect(Effect::grayscale())
+//     ->adjust(Adjust::tint(20, Color::MAGENTA))
+//     ->quality(Quality::auto())
+//     ->format(Format::auto()) . "\n");
 
 # Improve color, contrast, light
 
@@ -126,7 +127,7 @@ echo ($cloudinary->image('face')
 
 // echo ($cloudinary->image('living-room')
 //    ->resize(Resize::thumbnail(300,300,Gravity::auto()))
-//    ->adjust(Adjust::improve(30)->mode(Improve::INDOOR))
+//    ->adjust(Adjust::improve(30)->indoor())
 //    ->quality(Quality::auto())
 //    ->format(Format::auto()) . "\n");
 
@@ -140,64 +141,74 @@ echo ($cloudinary->image('face')
 //   . "\n");
 
 # cartoonify
-# use Cloudinary\Transformation\MiscEffectTrait;
-
+# notice the difference when you change the order of effect and crop
 // echo ($cloudinary->image('face')
 //   ->resize(Resize::thumbnail(300,300,Gravity::auto()))
 //   ->effect(Effect::cartoonify())
 //   ->quality(Quality::auto())
 //   ->format(Format::auto()) . "\n");
 
+// echo ($cloudinary->image('face')
+//   ->effect(Effect::cartoonify())
+//   ->resize(Resize::thumbnail(300,300,Gravity::auto()))
+//   ->quality(Quality::auto())
+//   ->format(Format::auto()) . "\n");
+
+# more on order makes a difference: crop first vs crop last
 // echo $cloudinary->image('face') 
 // -> effect(Effect::cartoonify())
 // -> roundCorners(CornerRadius::max())
 // -> effect(Effect::outline(100) -> color(Color::orange()))
 // -> background(Color::lightblue())
-// -> resize(Resize::scale() -> height(300)) . "\n";
+// -> resize(Resize::scale() -> height(300)) 
+// . "\n";
+
+// echo $cloudinary->image('face') 
+// -> resize(Resize::scale() -> height(300)) 
+// -> effect(Effect::cartoonify())
+// -> roundCorners(CornerRadius::max())
+// -> effect(Effect::outline(100) -> color(Color::orange()))
+// -> background(Color::lightblue())
+// . "\n";
 
 
 # Overlays
 
-# Text over image
+# Text over image - lots of options but fontFamily, fontSize and Text are required
+# default position is center
 
-echo ($cloudinary->image('faces')
-    ->resize(Resize::thumbnail(300, 300, Gravity::faces()))
-    ->overlay(
-        Overlay::source(Source::text('Tutoring')
-            // Source::text('Tutoring')
-            ->fontFamily('Arial')
-            ->fontSize(30)
-            ->fontWeight(FontWeight::BOLD) //weight is optional
-            ->effect(Effect::colorize()->color(Color::YELLOW))
-            ->effect(
-                Effect::outline(5)->color(Color::ORANGE)
-            ))
-            // Position::northWest()->x(10)->y(10)
-            ->position((new Position())
-                ->gravity(Gravity::compass(Compass::northWest()))
-                ->offsetX(10)->offsetY(10))
-    )
-
-    ->roundCorners(30)
-    ->format(Format::auto()) . "\n");
+// echo ($cloudinary->image('faces')
+//     ->resize(Resize::thumbnail(300, 300, Gravity::faces()))
+//     ->overlay(
+//         Overlay::source(Source::text('Tutoring')
+//             ->fontFamily('Arial')
+//             ->fontSize(30)
+//             ->fontWeight(FontWeight::BOLD) 
+//             ->effect(Effect::colorize()->color(Color::YELLOW))
+//             ->effect(
+//                 Effect::outline(5)->color(Color::ORANGE)
+//             ))
+//             ->position((new Position())
+//                 ->gravity(Gravity::compass(Compass::northWest()))
+//                 ->offsetX(10)->offsetY(10))
+//     )
+//     ->roundCorners(30)
+//     ->format(Format::auto()) . "\n");
 
 # Image over image
-
-echo ($cloudinary->image('working')
-    ->resize(Resize::scale(400))
-    //   ->overlay(
-    ->overlay(
-        Overlay::source(
-            Source::image('logo')
-                ->resize(Resize::thumbnail(50, 50))
-                ->adjust(Adjust::opacity(30))
-        )
-            ->position((new Position())
-                ->gravity(Gravity::compass(Compass::northEast()))
-                ->offsetX(10)->offsetY(10))
-        // Position::northEast()->x(10)->y(10)
-    )) . "\n";
-
+// echo ($cloudinary->image('working')
+//     ->resize(Resize::scale(400))
+//     ->overlay(
+//         Overlay::source(
+//             Source::image('logo')
+//                 ->resize(Resize::thumbnail(50, 50))
+//                 ->adjust(Adjust::opacity(50))
+//         )
+//         ->position((new Position())
+//             ->gravity(Gravity::compass(Compass::northEast()))
+//             ->offsetX(10)->offsetY(10))
+//     )
+// ) . "\n";
 
 
 # Text over video
@@ -205,26 +216,48 @@ echo ($cloudinary->image('working')
 // echo ($cloudinary->video('video')
 //   ->resize(Resize::scale(300))
 //   ->overlay(
-//     Source::text('Earth')
-//       ->fontFamily('Arial')
-//       ->fontSize(30)
-//       ->fontWeight(FontWeight::BOLD) //weight is optional
-//       ->effect(Effect::colorize()->color(Color::BLUE))
-//       ->effect(Effect::outline(5) -> color(Color::GREEN))
-//     ,
-//     Position::northWest()->x(10)->y(10)
+//     Overlay::VideoSource(VideoSource::text('Earth')
+//             ->fontFamily('Arial')
+//             ->fontSize(30)
+//             ->fontWeight(FontWeight::BOLD) 
+//             ->effect(Effect::colorize()->color(Color::BLUE))
+//             ->effect(Effect::outline(5) -> color(Color::GREEN)
+//     ))  
+//     ->position((new Position())
+//         ->gravity(Gravity::compass(Compass::northWest()))
+//         ->offsetX(10)->offsetY(10)
+//     )
 //   )
-// ). "\n";
+// ) . "\n";
+
+# from https://cloudinary.com/documentation/video_manipulation_and_delivery#adding_text_overlays
+use Cloudinary\Tag\VideoTag;
+use Cloudinary\Transformation\VideoTransformation;
+use Cloudinary\Transformation\Timeline;
+use Cloudinary\Transformation\TextStyle;
+
+
+echo (new VideoTag('video'))
+  ->overlay(Overlay::source(Source::text('Cool Video', (new TextStyle('arial', 60))))
+  // ->effect(Effect::colorize()->color(Color::BLUE))
+    ->position((new Position())
+      ->gravity(Gravity::compass(Compass::south()))
+      ->offsetY(80))
+    ->timeline(Timeline::position()->startOffset(2)->endOffset(5)));
+  
+
 
 # Image over Video
 
 // echo ($cloudinary->video('video')
 //   ->resize(Resize::scale(400))
 //   ->overlay(
-//     Source::image('logo')
+//     Overlay::Source(VideoSource::image('logo')
 //       ->resize(Resize::thumbnail(50, 50))
 //       ->adjust(Adjust::opacity(30))
-//     ,
-//     Position::northEast()->x(10)->y(10)
+//   )
+//   ->position((new Position())
+//                 ->gravity(Gravity::compass(Compass::northWest()))
+//                 ->offsetX(10)->offsetY(10))
 //   )
 // ). "\n";
