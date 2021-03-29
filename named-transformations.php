9546939c1123dee35f51f4f92ab583df1d4f6605
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-03-29 11:13:06 -0700

    update named transform syntax

--- named-transformations.php
+++ named-transformations.php
@@ -8,9 +8,7 @@ use Cloudinary\Transformation\Effect;
 use Cloudinary\Transformation\Gravity;
 use Cloudinary\Transformation\ImageTransformation;
 use Cloudinary\Transformation\Resize;
-use Cloudinary\Transformation\Source;
-use Cloudinary\Transformation\CornerRadius;
-use Cloudinary\Transformation\Position;
+use Cloudinary\Transformation\Format;
 
 # Config
 # Constructor
@@ -46,11 +44,11 @@ $api = $cloudinary->adminApi();
 
 # create a transformation variable
 
-$transformation = new ImageTransformation();
-$transformation
- ->resize(Resize::thumbnail(300, 300, Gravity::auto()))
- ->effect(Effect::grayscale())
- ->adjust(Adjust::tint(20, Color::MAGENTA));
+// $transformation = new ImageTransformation();
+// $transformation
+//  ->resize(Resize::thumbnail(300, 300, Gravity::auto()))
+//  ->effect(Effect::grayscale())
+//  ->adjust(Adjust::tint(20, Color::MAGENTA));
 
 # add the transformation to an image
 // echo ($cloudinary->image('face') -> addTransformation($transformation)) . "\n";
@@ -63,35 +61,34 @@ $transformation
 //  ->namedTransformation('duotone') . "\n";
 
 # here's a complex transformation
-echo $cloudinary->image('shirt_only.png')
-  ->overlay(
-    Source::image('logo')
-      ->resize(Resize::scale(300))
-      ->adjust(Adjust::brightness(-21))
-      ->roundCorners(CornerRadius::max()))
-
-      ->position((new Position())
-        ->offsetX(10)->offsetY(-200))
-
-  
-  ->overlay(
-    Source::text('Hello Jon')
-      ->fontFamily('Coustard')
-      ->fontSize(100)
-      ->fontWeight(FontWeight::BOLD)
-      ->resize(Resize::scale(365))
-      ->adjust(Adjust::opacity(70))
-      ->effect(Effect::colorize()->color(Color::rgb('#999999'))
-  )
-   ->position((new Position())
-                ->offsetX(-10))
-                . "\n";
+// echo $cloudinary->image('shirt_only.png')
+//  ->overlay(
+//   Overlay::source(
+//    Source::image('logo')
+//     ->resize(Resize::scale(300))
+//     ->adjust(Adjust::brightness(-21))
+//     ->roundCorners(CornerRadius::max())
+//   )->position((new Position())->offsetX(10)->offsetY(-200))
+//  )
+//  ->overlay(
+//   Overlay::source(
+//    Source::text('Hello Jon')
+//     ->fontFamily('Coustard')
+//     ->fontSize(100)
+//     ->fontWeight(FontWeight::BOLD)
+//     ->resize(Resize::scale(365))
+//     ->adjust(Adjust::opacity(70))
+//     ->effect(
+//      Effect::colorize()->color(Color::rgb('#999999'))
+//     )
+//   )->position((new Position())->offsetX(-10))
+//  ) . "\n";
 
 # create a named transformation for the complex transformation
 // echo json_encode($api->createTransformation('tshirt',
-// 'l_logo/c_scale,w_300/e_brightness:-21/r_max/fl_layer_apply,g_center,x_-10,y_-200/l_text:Coustard_100_bold:Hello Jon/c_scale,w_365/o_70/co_rgb:999999,e_colorize/fl_layer_apply,g_center,x_-10/f_auto')) . "\n";
+// 'l_logo/c_scale,w_300/e_brightness:-21/r_max/fl_layer_apply,x_10,y_-200/l_text:Coustard_100_bold:Hello Jon/c_scale,w_365/o_70/co_rgb:999999,e_colorize/fl_layer_apply,x_-10')) . "\n";
 
 # using named transform with f_auto: chain
-// echo $cloudinary->image('shirt_only.png')
-// ->namedTransformation('tshirt')
-// ->format(Format::auto()) . "\n";
+// echo $cloudinary->image('shirt_only')
+//  ->namedTransformation('tshirt')
+//  ->format(Format::auto()) . "\n";
