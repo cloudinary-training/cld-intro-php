71271b549cc2ebf6a086d978dcd7b1a93464cbfb
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-06-07 09:27:53 -0700

    a sample external config file

---
+++ config.sample.php
@@ -0,0 +1,14 @@
+<?php
+
+use Cloudinary\Configuration\Configuration;
+
+Configuration::instance(
+    [
+        "cloud" =>
+            [
+                'cloud_name' => 'CLOUD_NAME',
+                'api_key'    => 'API_KEY',
+                'api_secret' => 'API_SECRET',
+            ],
+    ]
+);
